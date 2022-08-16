f1caa4b57a1a927b71958b7295fd3b00f9c5b328
Dusan-O <dusan.orescanin@gmail.com>
2022-08-16 12:32:10 +0200

    Tap Bar Controller in Main & implementation of different Datas in Model file

---
+++ Top14_Rugby/Model/Club.swift
@@ -0,0 +1,19 @@
+//
+//  Club.swift
+//  Top14_Rugby
+//
+//  Created by Dusan Orescanin on 16/08/2022.
+//
+
+import Foundation
+
+
+struct Club {
+    var name: String
+    var nickname: String
+    var city: String
+    var stadium: Stadium
+    var desc: String
+    var europeanChampion: [Int]
+    var franceChampion: [Int]
+}
---
+++ Top14_Rugby/Model/Stadium.swift
@@ -0,0 +1,15 @@
+//
+//  Stadium.swift
+//  Top14_Rugby
+//
+//  Created by Dusan Orescanin on 16/08/2022.
+//
+
+import Foundation
+
+struct Stadium {
+    var name: String
+    var lat: Double
+    var lon: Double
+    var capacity: Int
+}
